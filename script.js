43e4e3f59eb73eeaee6189407476088269b67add
Lucas Simini <lucas.simini@hotmail.com>
2023-09-05 22:11:58 -0300

    adicionando ícones e interações

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-function mudarCor(){
-    let linkedin = document.getElementById('linkedin')
+//Mudança de Cor Botão Linkedin
+
+function onOverL(l){
+    l.setAttribute('src', './images/orange-icons/linkedin.png')
+}
+
+function onOutL(l){
+    l.setAttribute('src', './images/linkedin.png')
+}
+
+//Mudança de Cor Botão GitHub
+
+function onOverG(g){
+    g.setAttribute('src', './images/orange-icons/github.png')
+}
+
+function onOutG(g){
+    g.setAttribute('src', './images/github.png')
+}
+
+
+//Mudança de Cor Botão Whatsapp
+
+function onOverW(w){
+    w.setAttribute('src', './images/orange-icons/wpp.png')
+}
+
+function onOutW(w){
+    w.setAttribute('src', './images/wpp.png')
+}
+
+//Mudança de Cor Botão Instagram
+
+function onOverI(i){
+    i.setAttribute('src', './images/orange-icons/instagram.png')
+}
+
+function onOutI(i){
+    i.setAttribute('src', './images/instagram.png')
 }
